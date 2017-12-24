15889220d891bead5b0863a454866be0ecefe428
杨春来 <assionyang@126.com>
2017-12-24 21:45:47 +0800

    ad

--- ec-api-erp/src/main/java/com/yangcl/ec/api/erp/ErpApiApplication.java
+++ ec-api-erp/src/main/java/com/yangcl/ec/api/erp/ErpApiApplication.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrixDashboard
 @MapperScan("com.yangcl.ec.api.erp.mapper")
+@EnableTransactionManagement
 public class ErpApiApplication {
     public static void main(String[] args){
         SpringApplication.run(ErpApiApplication.class,args);
--- ec-api-erp/src/main/java/com/yangcl/ec/api/erp/service/erp/UserServiceImpl.java
+++ ec-api-erp/src/main/java/com/yangcl/ec/api/erp/service/erp/UserServiceImpl.java
@@ -4,6 +4,9 @@ import com.yangcl.ec.api.erp.mapper.UserMapper;
 import com.yangcl.ec.common.entity.erp.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service(value="userService")
 public class UserServiceImpl implements UserService {
@@ -11,6 +14,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT,timeout = 36000,rollbackFor = Exception.class)
     public User getByKey(long sysno) {
         return userMapper.getByKey(sysno);
     }
